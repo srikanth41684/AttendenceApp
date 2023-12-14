039d44c635b44da86baa455926104a528ba336d7
Usnagiri Srikanth <UsnagiriS@jmf.com>
2023-12-14 15:47:57 +0530

    employee type

--- src/main/screens/onBoarding/EmployeeType.js
+++ src/main/screens/onBoarding/EmployeeType.js
@@ -1,11 +1,130 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, SafeAreaView, TouchableWithoutFeedback} from 'react-native';
+import React, {useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
 
 const EmployeeType = () => {
+  const customNavigation = useNavigation();
+  const [commObj, setCommObj] = useState({
+    userType: null,
+  });
   return (
-    <View>
-      <Text>EmployeeType</Text>
-    </View>
+    <SafeAreaView
+      style={{
+        flex: 1,
+      }}>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: '#EFF1FE',
+          paddingHorizontal: 20,
+        }}>
+        <View
+          style={{
+            paddingTop: 20,
+          }}>
+          <Text
+            style={{
+              fontSize: 18,
+              lineHeight: 25,
+              color: '#000',
+              fontWeight: 'bold',
+            }}>
+            Select The Role
+          </Text>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+            gap: 20,
+            paddingTop: 20,
+          }}>
+          <TouchableWithoutFeedback
+            onPress={() => {
+              setCommObj(prev => ({
+                ...prev,
+                userType: 'manager',
+              }));
+            }}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: '#fff',
+                height: 150,
+                borderRadius: 10,
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}>
+              <Text
+                style={{
+                  fontSize: 16,
+                  lineHeight: 23,
+                  color: '#000',
+                  fontWeight: '500',
+                }}>
+                Manager
+              </Text>
+            </View>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback
+            onPress={() => {
+              setCommObj(prev => ({
+                ...prev,
+                userType: 'employee',
+              }));
+            }}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: '#fff',
+                height: 150,
+                borderRadius: 10,
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}>
+              <Text
+                style={{
+                  fontSize: 16,
+                  lineHeight: 23,
+                  color: '#000',
+                  fontWeight: '500',
+                }}>
+                Employee
+              </Text>
+            </View>
+          </TouchableWithoutFeedback>
+        </View>
+        <View
+          style={{
+            paddingBottom: 20,
+          }}>
+          <TouchableWithoutFeedback
+            onPress={() => {
+              if (commObj.userType !== null) {
+                customNavigation.navigate('home');
+              }
+            }}>
+            <View
+              style={{
+                backgroundColor: commObj.userType !== null ? '#3085FE' : 'red',
+                paddingVertical: 10,
+                alignItems: 'center',
+                borderRadius: 8,
+              }}>
+              <Text
+                style={{
+                  fontSize: 16,
+                  lineHeight: 23,
+                  color: '#fff',
+                  fontWeight: '500',
+                }}>
+                Next
+              </Text>
+            </View>
+          </TouchableWithoutFeedback>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
--- src/navigations/global-stack-nav.js
+++ src/navigations/global-stack-nav.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import OnBoardingStack from './onBoarding-stack-nav';
 import EmployeeType from '../main/screens/onBoarding/EmployeeType';
+import HomeScreen from '../main/screens/dashBoard/HomeScreen';
 
 const GlobalStack = createNativeStackNavigator();
 
@@ -16,6 +17,11 @@ const GlobalStackNav = () => {
       <GlobalStack.Screen
         name="employeeType"
         component={EmployeeType}
+        options={{headerShown: false}}
+      />
+      <GlobalStack.Screen
+        name="home"
+        component={HomeScreen}
         options={{headerShown: false}}
       />
     </GlobalStack.Navigator>
